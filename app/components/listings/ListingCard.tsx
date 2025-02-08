cd1973c8cb124bd1cb7c5cd84363954c52370683
M-Jawad338211 <jawad338211@gmail.com>
2025-02-08 17:45:46 +0500

    added favourite functionality

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,7 +4,12 @@ import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
+import { format } from 'date-fns';
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
+
 
 interface ListingCardProps {
     data: Listing;
@@ -56,9 +61,99 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
         const start = new Date(reservation.startDate);
         const end = new Date(reservation.endDate);
-    }, []);
+
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`
+    }, [reservation]); 
     return (
-        <div></div>
+        <div 
+        onClick={() => router.push(`/listings/${data.id}`)}
+        className="
+        col-span-1 cursor-pointer group        
+        "
+        >
+            <div className="flex flex-col gap-2 w-full">
+                {/* <div 
+                    className="
+                    aspect-square
+                    w-full
+                    relative
+                    overflow-hidden
+                    rounded-xl 
+                    "
+                >
+                    <Image 
+                        fill
+                        alt='Listing'
+                        src={data.imageSrc}
+                        className="
+                            object-cover
+                            h-full
+                            w-full
+                            group-hover:scale-110
+                            transition
+                        "                  
+                    />
+                </div> */}
+                <div 
+            className="
+                aspect-square
+                w-full
+                relative
+                overflow-hidden
+                rounded-xl 
+            "
+            >
+            {data.imageSrc ? (
+                <img
+                
+                alt='Listing'
+                src={data.imageSrc || '/default-image.jpg'}
+                className="
+                    object-cover
+                    h-full
+                    w-full
+                    group-hover:scale-110
+                    transition
+                "                  
+                />
+            ) : (
+                <div className="bg-gray-200 h-full w-full flex items-center justify-center text-gray-500">
+                No Image Available
+                </div>
+            )}
+                <div className="absolute top-3 right-3">
+                    <HeartButton
+                        listingId={data.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+            <div className="font-semibold text-lg">
+                {location?.region}, {location?.label}
+            </div>
+            <div className="font-light text-neutral-500">
+                {reservationDate || data.category}
+            </div>
+            <div className="flex flex-row items-center gap-1">
+                <div className="font-semibold">
+                    $ {price}
+                </div>
+                {!reservation && (
+                    <div className="font-light">night</div>
+                )}
+            </div>
+                {onAction && actionLabel && (
+                    <Button
+                        disabled={disabled}
+                        small
+                        label={actionLabel}
+                        onClick={handleCancel}
+                    />
+                )}
+
+            </div>
+
+        </div>
     )
 }
 export default ListingCard;
